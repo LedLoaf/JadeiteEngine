749fcff3860d5b6af7f1f433ac87a0e3deb7d71a
LedLoaf <LedLoaf@comcast.net>
2026-08-28 14:10:37 -0700

    Apply .gitignore to tracked files

--- build/CMakeFiles/jadeite.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for jadeite.
-# This may be replaced when dependencies are built.
--- build/CMakeFiles/jadeite.dir/src/rendering/default_shaders.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/jadeite.dir/src/rendering/default_shaders.cpp.o: \
-  C:\Users\LedLo\Downloads\SideScroller\src\rendering\default_shaders.cpp \
-  C:\Users\LedLo\Downloads\SideScroller\src\rendering\default_shaders.hpp
